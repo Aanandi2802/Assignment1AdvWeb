00665608e48a49804387630afb52fd9aa3f97bcc
Aanandi Pankhania <88778753+Aanandi2802@users.noreply.github.com>
2023-05-27 18:07:45 -0300

    Resolved deployment issues.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,9 +4,9 @@ import CustomNavbar from '../components/CustomNavbar'
 
 function Dashboard() {
   return (
-    <div>
-        Hii
-        </div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh" }}>
+      <h1>Welcome to the Admin panel of ClassMate</h1>
+    </div>
   )
 }
 
