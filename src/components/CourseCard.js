a79604abaa6e19cf11208f45e27e5b13d23c5cdf
Aanandi Pankhania <88778753+Aanandi2802@users.noreply.github.com>
2023-05-30 17:42:28 -0300

    Fixed errors

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { Card, Button, Modal, Form } from 'react-bootstrap';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState } from "react";
+import { Card, Button, Modal, Form } from "react-bootstrap";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const CourseCard = (props) => {
   const [showModal, setShowModal] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [startDate, setStartDate] = useState("");
 
   const handleCardClick = () => {
     setShowModal(true);
@@ -24,38 +25,43 @@ const CourseCard = (props) => {
     setShowModal(false);
   };
 
+  const handleStartDateChange = (e) => {
+    const selectedDate = e.target.value;
+    setStartDate(selectedDate);
+  };
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    
-    setShow(true)
-    setShowModal(false)
+    setShow(true);
+    setShowModal(false);
+  };
 
-  
-  }
-const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  const loremIpsum =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
   return (
     <div className="mt-5">
-<Card style={{ width: '18rem', ...props.style }}>
-      <Card.Img variant="top" src="https://picsum.photos/200" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-{loremIpsum}        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Instructor: {props.instructor}</ListGroup.Item>
-        <ListGroup.Item>Term: {props.term}</ListGroup.Item>
-        <ListGroup.Item>Start date: {props.startDate}</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Button variant="primary" onClick={handleCardClick}>Advanced options</Button>
+      <Card style={{ width: "18rem", ...props.style }}>
+        <Card.Img variant="top" src="https://picsum.photos/200" />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{loremIpsum}</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>Instructor: {props.instructor}</ListGroup.Item>
+          <ListGroup.Item>Term: {props.term}</ListGroup.Item>
+          <ListGroup.Item>Start date: {props.startDate}</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+          <Button variant="primary" onClick={handleCardClick}>
+            Advanced options
+          </Button>
+        </Card.Body>
+      </Card>
 
-      </Card.Body>
-    </Card>
-
-       <Modal show={showModal} onHide={handleModalClose}>
+      <Modal show={showModal} onHide={handleModalClose}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Details</Modal.Title>
         </Modal.Header>
@@ -63,34 +69,67 @@ const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typeset
           <Form>
             <Form.Group controlId="name">
               <Form.Label>Course name</Form.Label>
-              <Form.Control type="text" placeholder="Enter course name" value={props.title} onChange={e => setName(e.target.value)} />
+              <Form.Control
+                type="text"
+                placeholder="Enter course name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
             </Form.Group>
             <Form.Group controlId="email">
               <Form.Label>Instructor</Form.Label>
-              <Form.Control type="text" placeholder="Enter your email" value={props.instructor} onChange={e => setEmail(e.target.value)} />
+              <Form.Control
+                type="text"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </Form.Group>
             <Form.Group controlId="term">
               <Form.Label>Term</Form.Label>
-              <Form.Control type="text" placeholder="Enter your email" value={props.term} onChange={e => setEmail(e.target.value)} />
+              <Form.Control
+                as="select"
+                value={props.term}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              >
+                <option value="">Select term</option>
+                <option>Fall 23</option>
+                <option>Winter 23</option>
+                <option>Spring 23</option>
+                <option>Summer 23</option>
+              </Form.Control>
             </Form.Group>
             <Form.Group controlId="startDate">
-              <Form.Label>start date</Form.Label>
-              <Form.Control type="text" placeholder="Enter your email" value={props.startDate} onChange={e => setEmail(e.target.value)} />
+              <Form.Label>Start Date</Form.Label>
+              <Form.Control
+                type="date"
+                value={startDate}
+                onChange={handleStartDateChange}
+                required
+                min={new Date().toISOString().split("T")[0]}
+              />
             </Form.Group>
-
             <Form.Group controlId="Description">
               <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-          </Form.Group>
+              <Form.Control as="textarea" rows={3} required />
+            </Form.Group>
+
+            <Button variant="secondary" onClick={handleModalClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+            <Button variant="danger" onClick={handleShow}>
+              Remove course
+            </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleModalClose}>Close</Button>
-          <Button variant="primary" onClick={handleSaveClick}>Save</Button>
-          <Button variant="danger" onClick={handleShow}>Remove course</Button>
-
-        </Modal.Footer>
-      </Modal> 
+        
+      </Modal>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
--- src/views/Course.js
+++ src/views/Course.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-// import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import CourseCard from '../components/CourseCard';
@@ -7,9 +6,11 @@ import { Card, Button, Modal, Form } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function Course() {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [instructor, setInstructor] = useState('');
+  const [term, setTerm] = useState('');
+  const [startDate, setStartDate] = useState('');
 
   const handleCardClick = () => {
     setShowModal(true);
@@ -19,68 +20,89 @@ function Course() {
     setShowModal(false);
   };
 
-  const handleEditClick = () => {
-    // Perform any necessary actions when the edit button is clicked
-  };
+  const handleSaveClick = (e) => {
+    e.preventDefault(); // Prevent form submission
 
-  const handleSaveClick = () => {
-    // Perform any necessary actions when the save button is clicked
+    // Perform form validation
+    if (!name || !instructor || !term || !startDate) {
+      // Handle form validation error, display an error message, or prevent form submission
+      return;
+    }
+
+    // making sure the date is ahead of today's date.
+    const currentDate = new Date();
+    const selectedDate = new Date(startDate);
+
+    if (selectedDate <= currentDate) {
+      return;
+    }
+
+    // Perform save action
     setShowModal(false);
   };
+
   return (
-     <div> <div className="d-flex flex-column align-items-start" style={{marginLeft: '8%',marginRight: '8%', marginTop: "3%", marginBottom: '8%'}}>      
-              <div style={{fontSize:"32px", fontWeight: 700, marginBottom: "8px"}}>Courses</div>
+    <div>
+      <div className="d-flex flex-column align-items-start" style={{ marginLeft: '8%', marginRight: '8%', marginTop: '3%', marginBottom: '8%' }}>
+        <div style={{ fontSize: '32px', fontWeight: 700, marginBottom: '8px' }}>Courses</div>
 
         <div>
-            <Button variant="outline-primary" onClick={handleCardClick}>Create <FontAwesomeIcon icon={faPlus} /></Button>
+          <Button variant="outline-primary" onClick={handleCardClick}>
+            Create <FontAwesomeIcon icon={faPlus} />
+          </Button>
         </div>
 
-        <div className="d-flex  flex-wrap justify-content-between" style={{width: "100%"}}>
-            <CourseCard  title="data" code="CSCI 5409" instructor="Dr. dey" term="fall 23" startDate="23-03-2023" image="https://picsum.photos/200"/>
-            <CourseCard title="web" code="CSCI 5701" instructor="Dr Tushar" term="fall 23" startDate="23-04-2023"  image="https://picsum.photos/200"/>
-            <CourseCard title="ASDC" code="CSCI 5601" instructor="Dr Sharma" term="fall 23" startDate="23-07-2023" />
-            <CourseCard title="Networking" code="CSCI 5709" instructor="Dr Darshana" term="Summer 23" startDate="25-03-2023"/>
-
+        <div className="d-flex flex-wrap justify-content-between" style={{ width: '100%' }}>
+          <CourseCard title="data" code="CSCI 5409" instructor="Dr. dey" term="fall 23" startDate="23-03-2023" image="https://picsum.photos/200" />
+          <CourseCard title="web" code="CSCI 5701" instructor="Dr Tushar" term="fall 23" startDate="23-04-2023" image="https://picsum.photos/200" />
+          <CourseCard title="ASDC" code="CSCI 5601" instructor="Dr Sharma" term="fall 23" startDate="23-07-2023" />
+          <CourseCard title="Networking" code="CSCI 5709" instructor="Dr Darshana" term="Summer 23" startDate="25-03-2023" />
         </div>
-    </div>
+      </div>
 
-    <Modal show={showModal} onHide={handleModalClose}>
+      <Modal show={showModal} onHide={handleModalClose}>
         <Modal.Header closeButton>
           <Modal.Title>Create course</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSaveClick}>
             <Form.Group controlId="name">
               <Form.Label>Course name</Form.Label>
-              <Form.Control type="text" placeholder="Enter course name" value={""} onChange={e => setName(e.target.value)} />
+              <Form.Control type="text" placeholder="Enter course name" value={name} onChange={(e) => setName(e.target.value)} required />
             </Form.Group>
-            <Form.Group controlId="email">
+            <Form.Group controlId="instructor">
               <Form.Label>Instructor</Form.Label>
-              <Form.Control type="text" placeholder="Enter Instructor name" value={""} onChange={e => setEmail(e.target.value)} />
+              <Form.Control type="text" placeholder="Enter Instructor name" value={instructor} onChange={(e) => setInstructor(e.target.value)} required />
             </Form.Group>
             <Form.Group controlId="term">
               <Form.Label>Term</Form.Label>
-              <Form.Control type="text" placeholder="Enter Term" value={""} onChange={e => setEmail(e.target.value)} />
+              <Form.Select value={term} onChange={(e) => setTerm(e.target.value)} required>
+                <option value="">Select term</option>
+                <option value="fall 23">Fall 23</option>
+                <option value="summer 23">Summer 23</option>
+              </Form.Select>
             </Form.Group>
             <Form.Group controlId="startDate">
-              <Form.Label>start date</Form.Label>
-              <Form.Control type="text" placeholder="Enter start date" value={""} onChange={e => setEmail(e.target.value)} />
+              <Form.Label>Start date</Form.Label>
+              <Form.Control type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} required min={new Date().toISOString().split('T')[0]} />
             </Form.Group>
 
-            <Form.Group controlId="Description">
+            <Form.Group controlId="description">
               <Form.Label>Description</Form.Label>
               <Form.Control as="textarea" rows={3} />
-          </Form.Group>
+            </Form.Group>
+            <div className="mt-3">
+            <Button variant="secondary" onClick={handleModalClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button></div>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleModalClose}>Close</Button>
-          <Button variant="primary" onClick={handleSaveClick}>Save</Button>
-        </Modal.Footer>
-      </Modal> 
-
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default Course
+export default Course;
